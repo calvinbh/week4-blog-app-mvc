a8eba7d41f5f867fb428a8c010600a377d093468
Calvin Williams <calvinbh@gmail.com>
2015-02-14 18:53:19 -0700

    initial commit

---
+++ Blog_App_MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Blog_App_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_App_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Db db = Db.Instance;
+        public ActionResult Index()
+        {
+            return View(db.posts);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Details(int id = -1)
+        {
+            if (id == -1) return RedirectToAction("Index");
+            {
+                Post model = db.posts.Where(x => x.ID == id).FirstOrDefault();
+
+                return View(model);
+            }
+
+        }
+    }
+}
---
+++ Blog_App_MVC/Models/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog_App_MVC.Models
+{
+    public class Post
+    {
+        public string Title {get; set;}
+        public string Body { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public int ID { get; set; }
+
+        public List<string> Comment {get; set;}
+
+        public Post(string title, string body, DateTime createdOn, string createdBy, int id, List<string> comment)
+        {
+            Title = title;
+            Body = body;
+            CreatedOn = createdOn;
+            CreatedBy = createdBy;
+            ID = id;
+            Comment = comment;
+
+        }
+
+        public Post()
+        { 
+        }
+
+    }
+}
